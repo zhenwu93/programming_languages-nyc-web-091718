1dead9588ee175f6920cb67dca9d2cdfb27bd2ac
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 03:21:53 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,18 @@
 def reformat_languages(languages)
   new_hash = {}
-   languages.each do |style,language|
-     language.each do |name,type|
+   languages.each do |style, language|
+     binding.pry
+     language.each do |name, type|
+       binding.pry
          if new_hash[name] == nil
            new_hash[name] = type
            new_hash[name][:style] = []
+           binding.pry
          end
          new_hash[name][:style] << style
+         binding.pry
      end
    end
    new_hash
+   binding.pry
  end
