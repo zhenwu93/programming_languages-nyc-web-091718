3a4b28bdaa1a7cb8d9fb53de91a32fe330afbfb5
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 02:39:49 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |diff_languages, languages|
+    languages.each do |languages, language|
+      language.each do |key, value|
+        new_hash[diff_languages] = {
+          language = {language:value}
+        }
+
+        if languages[:diff_languages] == :oo
+        elsif languages[:diff_languages] == :functional
+        else puts "invalid"
+        end
+      end
+    end
+  end
 end
