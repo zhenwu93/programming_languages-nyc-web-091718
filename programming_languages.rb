89e83179bb342439f056b6571fa38f885536c312
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 02:54:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,5 +3,20 @@ def reformat_languages(languages_hash)
 
   languages_hash.each do |language_category, languages|
     languages.each do |language, attributes|
-      if language ==
+      attributes.each do |attribute, value|
+        if language == :javascript
+          new_hash[language] == {
+            :type => value,
+            :style => [:oo, :functional]
+          }
+        else 
+          new_hash[language] = {
+            :type => value,
+            :style => [language_category]
+          }
+        end
+      end
+    end
+  end
+  new_hash
 end
