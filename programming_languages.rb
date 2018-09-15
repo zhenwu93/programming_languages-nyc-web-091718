5e42c7fb46474088529c6669582c1e73658414bc
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 02:50:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,6 +2,6 @@ def reformat_languages(languages_hash)
   new_hash = {}
 
   languages_hash.each do |language_category, languages|
-    languages.each do |language_type, language|
+    languages.each do |language, attributes|
       if language ==
 end
